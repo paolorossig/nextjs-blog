cd3a12185ec77c0fe2628c416887ae262e5c9074
PaoloRossi <paolo.rossi@pucp.edu.pe>
2021-10-05 00:54:21 -0500

    Adding Typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,13 @@ import Layout, { siteTitle } from "../components/layout";
 import Date from "../components/date";
 import getSortedPostsData from "../lib/posts";
 import utilStyles from "../styles/utils.module.css";
+import { GetStaticProps } from "next";
 
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData,
+}: {
+  allPostsData: { date: string; title: string; id: string }[];
+}) {
   return (
     <Layout home>
       <Head>
@@ -34,11 +39,11 @@ export default function Home({ allPostsData }) {
   );
 }
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData();
   return {
     props: {
       allPostsData,
     },
   };
-}
+};
